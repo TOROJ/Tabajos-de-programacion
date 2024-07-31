c50472aaa56b6a57bc0a9976913e1a726504a692
TOROJ <jutororo@unal.edu.co>
2024-07-31 15:53:39 -0500

    ejercicio3.py

--- ejercicio3.py
+++ ejercicio3.py
@@ -1,11 +1,11 @@
-
-v=[2,3,2]
-W=[2,1,2]
-p=[]
-for i in range(len(v)):
-    m=v[i]*W[i]
-    p.append(m)
-print(p)
+def matris(v,W):
+ v=[2,3,2]
+ W=[2,1,2]
+ p=[]
+    for i in range(len(v)):
+       m=v[i]*W[i]
+       p.append(m)
+    print(p)
     
 
 
